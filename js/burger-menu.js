4c25c8fb56ebf9d4106a595970135ec85995e4db
RemonJoL <83402319+RemonJoL@users.noreply.github.com>
2021-09-24 15:26:34 +0100

    Multiple fixes
    - Fixed partner section for IE
    - Cleaned up code for partners, carousel and burger menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,7 +1,9 @@
 document.querySelector(".burger-button").addEventListener("click", function(){
   document.querySelector(".burger-overlay").style.visibility = "visible"
+  document.body.style.overflow = "hidden";
 });
 
 document.querySelector(".burger-overlay").addEventListener("click", function(){
     document.querySelector(".burger-overlay").style.visibility = "hidden"
+    document.body.style.overflow = "auto";
 });
